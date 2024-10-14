ffbddff8552ad453a384f64a072b90a4cd63960f
Lukas Lopatovsky <lopatovsky@gmail.com>
2024-10-14 17:14:01 +0200

    Clean and comment on build_model function

--- rlmodule/source/build.py
+++ rlmodule/source/build.py
@@ -1,20 +1,18 @@
 from rlmodule.source.rlmodel_cfg import BaseRLCfg, RLModelCfg, SharedRLModelCfg
 from rlmodule.source.utils import get_output_size
 
-#TODO this, or inside RLModel class that have a config passed
 def build_model(cfg: BaseRLCfg):
-
-    #1) network = create # for now it is created, todo from config
     
-    #2) get output size - what type?  ..what is Model init doing with action/observation space
-
+    # build base network of function approximator
     net = cfg.network.module(cfg.network)
     
+    # get output size to be used as output layer input
     network_output_size = get_output_size(net, cfg.network.input_size)
 
     def build_output_layer(layer_cfg):
         return layer_cfg.class_type( device = cfg.device, input_size = network_output_size, cfg = layer_cfg)
     
+    # build function approximator
     if isinstance(cfg, RLModelCfg):
         rl_model = cfg.class_type( device = cfg.device,
                         network = net,
